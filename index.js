682e1f2b3de20cb87ab70ec16d6018af0d718670
CAREFREE-VILLAGER <as314202@gmail.com>
2024-09-12 13:07:24 +0530

    making some changes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const registerRoute = require('./routes/registerRoute');
@@ -6,6 +5,7 @@ const loginRoute = require('./routes/loginRoute');
 const dotenv = require('dotenv').config();
 const logoutRoute = require('./routes/logoutRoute');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');  // Import connect-mongo
 const productRoutes = require('./routes/productRoutes'); 
 const cartRoute = require('./routes/cartRoute');
 const cors = require('cors');
@@ -14,13 +14,17 @@ const app = express();
 const PORT = 8080;
 app.use(cors());
 
-
 app.use(express.json());
 
+// Use connect-mongo to store sessions in MongoDB
 app.use(session({
     secret: 'your-session-secret',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI || "mongodb+srv://demonking:demon123@cluster0.pmncnmh.mongodb.net/?retryWrites=true&w=majority", // Store session data in MongoDB
+        ttl: 14 * 24 * 60 * 60 // Session expiration time (14 days)
+    })
 }));
 
 app.use(bodyParser.json());
@@ -33,31 +37,29 @@ app.use(logoutRoute);
 app.get('/', (req, res) => {
     res.send('Welcome to the home page!');
 });
-app.listen(8080, () => {
+
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-
 const mongoose = require('mongoose');
 
+const mongoUrl = process.env.MONGODB_URI || "mongodb+srv://demonking:demon123@cluster0.pmncnmh.mongodb.net/?retryWrites=true&w=majority";
 
-const mongoUrl = process.env.MONGODB_URI;
 const config = {
     connectTimeoutMS: 30000,
     socketTimeoutMS: 30000,
-    useUnifiedTopology: true
-  }
+    useNewUrlParser: true,  // Keep only the useNewUrlParser option
+    // Remove useUnifiedTopology
+};
 
-//   const connectDB = async ()=>{
-//     try{
-//         await mongoose.connect(mongoUrl,config);
-//         console.log('MongoDB connected');
-//     }catch(err){
-//         console.log("MongoDB connection error:",err);
-//     }
-//   };
-mongoose.connect("mongodb+srv://demonking:demon123@cluster0.pmncnmh.mongodb.net/?retryWrites=true&w=majority",config).then(() => {
-    console.log('MongoDB connected');
-}).catch(err => console.error('MongoDB connection error:', err));
+// Connect to MongoDB using Mongoose
+mongoose.connect(mongoUrl, config)
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+    });
 
-module.exports= mongoose.connection;
+module.exports = mongoose.connection;
